4bbd663ae2cde59be07080b281fb1a91552e7eff
mohemohe <mohemohe@ghippos.net>
2019-05-06 13:00:33 +0900

    #3 add some tests

--- s3fs_test.go
+++ s3fs_test.go
@@ -51,6 +51,19 @@ func TestS3FS_Put(t *testing.T) {
 	}
 }
 
+func TestS3FS_Info(t *testing.T) {
+	t.Run("file", func(st *testing.T) {
+		info := fs.Info("/testfile")
+		if info == nil {
+			st.Fatal("s3 error")
+		}
+		length := *info.ContentLength
+		if length != int64(len([]byte("this is test string"))) {
+			st.Fatal("io error")
+		}
+	})
+}
+
 func TestS3FS_Get(t *testing.T) {
 	t.Run("exists", func(st *testing.T){
 		readCloser, err := fs.Get("/testfile")
@@ -161,79 +174,121 @@ func TestS3FS_MkDir(t *testing.T) {
 func TestS3FS_Copy(t *testing.T) {
 	t.Run("single copy", func(st *testing.T) {
 		if err := fs.Copy("/testfile", "/testdir1/testfile", nil); err != nil {
-			t.Fatal("copy error:", err)
+			st.Fatal("copy error:", err)
 		}
 		readCloser, err := fs.Get("/testdir1/testfile")
 		if err != nil {
-			t.Fatal("get file error:", err)
+			st.Fatal("get file error:", err)
 		}
 		buf := new(bytes.Buffer)
 		if _, err := buf.ReadFrom(*readCloser); err != nil {
-			t.Fatal("io error:", err)
+			st.Fatal("io error:", err)
 		}
 		body := buf.String()
 		if body != "this is test string" {
-			t.Fatal("invalid data")
+			st.Fatal("invalid data")
+		}
+	})
+	t.Run("single copy with metadata", func(st *testing.T) {
+		metadataKey := "Test-Metadata"
+		metadataValue := "test"
+		if err := fs.Copy("/testfile", "/testmetadata", &map[string]*string{
+			metadataKey: &metadataValue,
+		}); err != nil {
+			st.Fatal("copy error:", err)
+		}
+		readCloser, err := fs.Get("/testdir1/testfile")
+		if err != nil {
+			st.Fatal("get file error:", err)
+		}
+		buf := new(bytes.Buffer)
+		if _, err := buf.ReadFrom(*readCloser); err != nil {
+			st.Fatal("io error:", err)
+		}
+		body := buf.String()
+		if body != "this is test string" {
+			st.Fatal("invalid data")
+		}
+		info := fs.Info("/testmetadata")
+		if info == nil {
+			st.Fatal("s3 info error")
+		}
+		s3Metadata := *info.Metadata[metadataKey]
+		if s3Metadata != metadataValue {
+			st.Fatal("s3 metadata error:", s3Metadata)
 		}
 	})
 	t.Run("bulk copy", func(st *testing.T) {
 		if err := fs.MkDir("/bulkcopy_a"); err != nil {
-			t.Fatal(err)
+			st.Fatal(err)
 		}
 		if err := fs.Copy("/testfile", "/bulkcopy_a/testfile", nil); err != nil {
-			t.Fatal("copy error:", err)
+			st.Fatal("copy error:", err)
 		}
 		if err := fs.MkDir("/bulkcopy_b"); err != nil {
-			t.Fatal("mkdir error:", err)
+			st.Fatal("mkdir error:", err)
 		}
 		if err := fs.Copy("/bulkcopy_a/", "/bulkcopy_b/", nil); err != nil {
-			t.Fatal("copy error:", err)
+			st.Fatal("copy error:", err)
 		}
 
 		readCloser, err := fs.Get("/bulkcopy_b/bulkcopy_a/testfile")
 		if err != nil {
-			t.Fatal("get file error:", err)
+			st.Fatal("get file error:", err)
 		}
 		buf := new(bytes.Buffer)
 		if _, err := buf.ReadFrom(*readCloser); err != nil {
-			t.Fatal("io error:", err)
+			st.Fatal("io error:", err)
 		}
 		body := buf.String()
 		if body != "this is test string" {
-			t.Fatal("invalid data")
+			st.Fatal("invalid data")
 		}
 	})
 }
 
 func TestS3FS_Move(t *testing.T) {
-	beforeList := fs.List("/testdir1")
-	if beforeList == nil {
-		t.Fatal("cannot connect s3")
-	}
-
-	if err := fs.Move("/testdir1/", "/testdir2/"); err != nil {
-		t.Fatal("move error:", err)
-	}
-
-	afterList := fs.List("/testdir2/testdir1")
-	if afterList == nil {
-		t.Fatal("cannot connect s3")
-	}
-
-	if len(*beforeList) != len(*afterList) {
-		t.Fatal("invalid files:", *beforeList, *afterList)
-	}
-	for i := range *beforeList {
-		if (*beforeList)[i].Name != (*afterList)[i].Name {
-			t.Fatal("name error:", (*beforeList)[i].Name, (*afterList)[i].Name)
+	t.Run("single", func(st *testing.T) {
+		if err := fs.MkDir("/singlemove"); err != nil {
+			st.Fatal("mkdir error:", err)
 		}
-		if (*beforeList)[i].Size != (*afterList)[i].Size {
-			t.Fatal("size error:", (*beforeList)[i].Size, (*afterList)[i].Size)
+		if err := fs.Copy("/testfile", "/movetest", nil); err != nil {
+			st.Fatal("cp error:", err)
 		}
-		if (*beforeList)[i].Type != (*afterList)[i].Type {
-			t.Fatal("type error:", (*beforeList)[i].Type, (*afterList)[i].Type)
+		if err := fs.Move("/movetest", "/singlemove/movetest"); err != nil {
+			st.Fatal("mv error:", err)
 		}
-	}
+	})
+	t.Run("bulk", func(st *testing.T) {
+		beforeList := fs.List("/testdir1")
+		if beforeList == nil {
+			st.Fatal("cannot connect s3")
+		}
+
+		if err := fs.Move("/testdir1/", "/testdir2/"); err != nil {
+			st.Fatal("move error:", err)
+		}
+
+		afterList := fs.List("/testdir2/testdir1")
+		if afterList == nil {
+			st.Fatal("cannot connect s3")
+		}
+
+		if len(*beforeList) != len(*afterList) {
+			st.Fatal("invalid files:", *beforeList, *afterList)
+		}
+		for i := range *beforeList {
+			if (*beforeList)[i].Name != (*afterList)[i].Name {
+				st.Fatal("name error:", (*beforeList)[i].Name, (*afterList)[i].Name)
+			}
+			if (*beforeList)[i].Size != (*afterList)[i].Size {
+				st.Fatal("size error:", (*beforeList)[i].Size, (*afterList)[i].Size)
+			}
+			if (*beforeList)[i].Type != (*afterList)[i].Type {
+				st.Fatal("type error:", (*beforeList)[i].Type, (*afterList)[i].Type)
+			}
+		}
+	})
 }
 
 func TestS3FS_Delete(t *testing.T) {
