ce667c8992b8d0d6bec1f2bc3b5c07e08c54d9ed
Tommi Vepsae <tommi.vepsae@mobilus.co.jp>
2019-09-20 17:56:46 +0900

    Instead of replacing, add ExactPathExists

--- s3fs.go
+++ s3fs.go
@@ -400,6 +400,22 @@ func (this *S3FS) PathExists(key string) bool {
 		Bucket:    aws.String(this.config.Bucket),
 		Prefix:    aws.String(this.getKey(key)),
 		Delimiter: aws.String("/"),
+		MaxKeys:   aws.Int64(1),
+	})
+	if err != nil {
+		return false
+	}
+	if *list.KeyCount > 0 {
+		return true
+	}
+	return false
+}
+
+func (this *S3FS) ExactPathExists(key string) bool {
+	list, err := this.s3.ListObjectsV2(&s3.ListObjectsV2Input{
+		Bucket:    aws.String(this.config.Bucket),
+		Prefix:    aws.String(this.getKey(key)),
+		Delimiter: aws.String("/"),
 	})
 	if err != nil {
 		return false
